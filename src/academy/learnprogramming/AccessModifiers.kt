1ebca6160a8f685b9a36594fc49a3ba29b80dbaf
Vkashaykova <vkashaykova@gmail.com>
2024-05-31 18:47:07 +0300

    Initial commit

---
+++ src/academy/learnprogramming/AccessModifiers.kt
@@ -0,0 +1,32 @@
+package academy.learnprogramming
+
+
+fun main(args: Array<String>) {
+
+
+    val emp = Employee("John")
+
+    println(emp.firstName)
+    println(emp.fullTime)
+
+    val emp2 = Employee("Joe")
+    println(emp2.firstName)
+    println(emp2.fullTime)
+
+    val emp3 = Employee("Jane", false)
+    println(emp3.firstName)
+    println(emp3.fullTime)
+    println(Demo().dummy)
+}
+
+class Employee(val firstName: String, var fullTime: Boolean = true) {
+
+}
+
+class Demo{
+    val dummy: String
+
+    constructor() {
+        dummy = "Hello"
+    }
+}
