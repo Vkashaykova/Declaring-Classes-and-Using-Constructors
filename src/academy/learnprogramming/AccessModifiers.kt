be4daee85e614a2a852a12f5ae3a4aa9cd670924
Vkashaykova <vkashaykova@gmail.com>
2024-06-03 19:01:36 +0300

    commit properties and backing fields

--- src/academy/learnprogramming/AccessModifiers.kt
+++ src/academy/learnprogramming/AccessModifiers.kt
@@ -19,8 +19,16 @@ fun main(args: Array<String>) {
     println(Demo().dummy)
 }
 
-class Employee(val firstName: String, var fullTime: Boolean = true) {
-
+class Employee(val firstName: String,  fullTime: Boolean = true) {
+ var fullTime=fullTime
+    get() {
+        println("Running the custom get")
+        return field
+    }
+    set(value) {
+        println("Running the custom set")
+        field = value
+    }
 }
 
 class Demo{
